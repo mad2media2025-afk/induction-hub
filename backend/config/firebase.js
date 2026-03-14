464fd424fa48fa44aec4f05a83cc5f8b7c42be69
retarduser404 <kartikpathak883@gmail.com>
2026-03-14 14:34:24 +0530

    fix: robust firebase private key parsing for production

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -12,8 +12,10 @@ function initFirebase() {
     type: 'service_account',
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    // The \n in .env needs to be converted to actual newlines
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    // More robust parsing for the private key
+    private_key: process.env.FIREBASE_PRIVATE_KEY 
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"(.*)"$/, '$1')
+      : undefined,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
